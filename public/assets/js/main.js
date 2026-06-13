6ef58f6439062345c45e555c578144da76b119e1
renjiyomo <markerickserrano1@gmail.com>
2026-06-13 23:21:36 +0800

    updated the roles

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Select the floating chat button
     const chatBtn = document.querySelector('button.fixed');
     
     if(chatBtn) {
         chatBtn.addEventListener('click', () => {
-            // In the future, you can trigger a modal or open an email draft here
             window.location.href = "mailto:email@example.com?subject=Let's Connect";
         });
     }
