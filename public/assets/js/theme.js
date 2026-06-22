06dc00b08ee8e4d0811e27aebe61ca0f3afbf6fe
renjiyomo <markerickserrano1@gmail.com>
2026-06-22 22:48:16 +0800

    updated Resume

--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -1,32 +1,32 @@
 const htmlElement = document.documentElement;
-        const themeToggleBtn = document.getElementById('theme-toggle');
-        const profileImage = document.getElementById('profile-image');
+const themeToggleBtn = document.getElementById('theme-toggle');
+const profileImage = document.getElementById('profile-image');
         
-        const lightModeImage = 'assets/images/light-profile.jpg';
-        const darkModeImage = 'assets/images/dark-profile.jpg';
+const lightModeImage = 'assets/images/light-profile.jpg';
+const darkModeImage = 'assets/images/dark-profile.jpg';
 
-        if (localStorage.theme === 'dark' || !('theme' in localStorage)) {
-            htmlElement.classList.add('dark');
+if (localStorage.theme === 'dark' || !('theme' in localStorage)) {
+    htmlElement.classList.add('dark');
+     profileImage.src = darkModeImage;
+} else {
+    htmlElement.classList.remove('dark');
+    profileImage.src = lightModeImage;
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    htmlElement.classList.toggle('dark');
+    const isDark = htmlElement.classList.contains('dark');
+
+    profileImage.style.opacity = 0;
+            
+    setTimeout(() => {
+        if (isDark) {
+            localStorage.theme = 'dark';
             profileImage.src = darkModeImage;
         } else {
-            htmlElement.classList.remove('dark');
+            localStorage.theme = 'light';
             profileImage.src = lightModeImage;
         }
-
-        themeToggleBtn.addEventListener('click', () => {
-            htmlElement.classList.toggle('dark');
-            const isDark = htmlElement.classList.contains('dark');
-
-            profileImage.style.opacity = 0;
-            
-            setTimeout(() => {
-                if (isDark) {
-                    localStorage.theme = 'dark';
-                    profileImage.src = darkModeImage;
-                } else {
-                    localStorage.theme = 'light';
-                    profileImage.src = lightModeImage;
-                }
-                profileImage.style.opacity = 1;
-            }, 150);
-        });
+        profileImage.style.opacity = 1;
+    }, 150);
+});
